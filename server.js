4b59cf52328d7e326924cad882d9dfdc4d553822
xtheslayer666 <73895283+xtheslayer666@users.noreply.github.com>
2020-11-10 09:45:33 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -108,9 +108,9 @@ function getOpeningHours() {
   xhttp.setRequestHeader("Content-Type", "application/json");
   xhttp.send(null);
   xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200 ) {
+  if (this.readyState == 4 && (this.status >= 200 && this.status < 400 )) {
   // Typical action to be performed when the document is ready:
-  end = JSON.parse(xhttp.responseText);
+  end = JSON.parse(xhttp.response);
   answer = end[1].Dienstag.toString();
 
   // const openings = data;
